483687e59a3b3a23a1a18dc70faf106908bf4380
GR <gouramichelle24@gmail.com>
2025-07-19 04:03:47 -0400

    modificaciones

--- facturacion/src/main/java/com/ecomarket/facturacion/FacturacionDataLoader.java
+++ facturacion/src/main/java/com/ecomarket/facturacion/FacturacionDataLoader.java
@@ -6,6 +6,7 @@ import net.datafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.time.LocalDate;
 import java.util.Locale;
